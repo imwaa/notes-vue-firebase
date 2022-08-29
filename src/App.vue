cfd8f1c11975cf99168a23f423a03aee676a2f4d
walid salhi <walid.dev@outlook.fr>
2022-08-29 13:59:07 +0200

    ui finish, added firebase to project

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,31 @@
 <template>
   <div class="badass-todo mt-5">
     <div class="title has-text-centered">Vue.js Todo</div>
-    <div class="field is-grouped mb-5">
-      <p class="control is-expanded">
-        <input class="input" type="text" placeholder="Ajouter une tache">
-      </p>
-      <p class="control">
-        <a class="button is-primary">
-          Ajouter
-        </a>
-      </p>
-    </div>
-    <div v-for="todo in todos" class="card mb-5">
+    <form @submit.prevent="addTodo">
+      <div class="field is-grouped mb-5">
+        <p class="control is-expanded">
+          <input v-model="newTodoContent" class="input" type="text" placeholder="Ajouter une tache">
+        </p>
+        <p class="control">
+          <button class="button is-primary" :disabled="!newTodoContent">
+            Ajouter
+          </button>
+        </p>
+      </div>
+    </form>
+
+    <div v-for="todo in todos" class="card mb-5" :class="{ 'has-background-success-light': todo.done }">
       <div class="card-content">
         <div class="content">
-          <div class="todo-title">
-            {{todo.content}}
+          <div class="todo-title" :class="{ 'has-text-success line-through': todo.done }">
+            {{ todo.content }}
           </div>
-          <div class="todo-actionsbuttons">
-            <button class="button is-small is-primary is-light">
+          <div class="todo-actions buttons">
+            <button class="button is-small is-primary is-light" :class="todo.done ? 'is-success' : 'is-light'"
+              @click="toggleDone(todo.id)">
               &check;
             </button>
-            <button class="button is-small is-danger is-light ml-2">
+            <button @click="deleteTodo(todo.id)" class="button is-small is-danger is-light ml-2">
               &cross;
             </button>
           </div>
@@ -45,28 +49,47 @@
   justify-content: space-between;
   align-items: center;
 }
+
+.line-through {
+  text-decoration: line-through;
+}
 </style>
 
 <script setup>
 
-  import { ref } from 'vue';
-  
-  const todos = ref([
-    {
-      id:'id1',
-      content: 'Lire un livre',
-      done: false
-    },
-    {
-      id:'id2',
-      content: 'Lire un livre',
-      done: false
-    },
-    {
-      id:'id3',
-      content: 'Lire un livre',
-      done: false
-    }
-  ])
+import { ref } from 'vue';
+import { v4 as uuidv4 } from 'uuid';
+
+const todos = ref([
+])
+
+//todo to be add
+const newTodoContent = ref('')
+
+// Add todo
+const addTodo = () => {
+  const newTodo = {
+    id: uuidv4(),
+    content: newTodoContent.value,
+    done: false
+  }
+  todos.value.unshift(newTodo)
+  newTodoContent.value = ''
+}
+
+
+//delete todo
+const deleteTodo = id => {
+  todos.value = todos.value.filter(todo => {
+    todo.id !== id
+  })
+}
+
+//marking done
+const toggleDone = id => {
+  console.log()
+  const index = todos.value.findIndex(todo => todo.id === id)
+  todos.value[index].done = !todos.value[index].done
+}
 
 </script>
---
+++ src/firebase/index.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDaozmWq7WEzCcrPcgwBjjlp8hLJEws64s",
+  authDomain: "vue3-todo-12726.firebaseapp.com",
+  projectId: "vue3-todo-12726",
+  storageBucket: "vue3-todo-12726.appspot.com",
+  messagingSenderId: "345453505761",
+  appId: "1:345453505761:web:920e60cb7f8528c660dba0",
+  measurementId: "G-B7W3JVXC8K",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
